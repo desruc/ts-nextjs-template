ff8c66e8f9aab500702e55f05101f2dd0d3d4f87
James Cameron <jdonaldcameron@gmail.com>
2020-07-20 16:48:12 +1000

    feat: init typescript, next and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  GetStaticProps,
+  NextPage,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+} from 'next';
+
+interface IndexProps {
+  greeting: string;
+}
+
+const IndexPage: NextPage<IndexProps> = (props: IndexProps) => {
+  const { greeting } = props;
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext,
+): Promise<GetStaticPropsResult<IndexProps>> => ({
+  props: {
+    greeting: 'Hello World!',
+  },
+});
+
+export default IndexPage;
